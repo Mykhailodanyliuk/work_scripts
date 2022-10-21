76d2ed0d856f829cc4fb468ca07f50dbad8a2c52
Mykhailo Danyliuk <mihailodaniliuk@gmail.com>
2022-10-21 19:08:37 +0300

    v 1.1.2

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
+import time
+
 import pymongo
 
 client = pymongo.MongoClient('mongodb://localhost:27017')
 db = client['db']
+updated_clinical_trials = db['updated_clinical_trials']
 collection = db['clinical_trials']
 organizations_collection = db['clinical_trials_organizations']
 organizations = collection.distinct(
     key='FullStudy.Study.ProtocolSection.IdentificationModule.Organization.OrgFullName')
 for organization in list(organizations):
-    organizations_collection.insert_one({"organization":organization})
+    # organizations_collection.insert_one({"organization": organization})
+    list_organization_trials = list(collection.find({'FullStudy.Study.ProtocolSection.IdentificationModule.Organization.OrgFullName': organization}))
+    for i in list_organization_trials:
+        print(i)
+    updated_clinical_trials.insert_one({'organization': organization, 'Organization_trials': list_organization_trials})
